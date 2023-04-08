8b84152fee7a8a35af3f1170c50609b6bcc66d0b
TGR <account@thomasreddy.com>
2023-04-08 09:28:55 +0100

    doc and test

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,110 @@
 #![feature(adt_const_params)]
 
 pub mod undirected_graph;
+pub mod undirected_sparse_graph;
 
+pub use undirected_graph::UndirectedGraph;
+pub use undirected_sparse_graph::UndirectedSparseGraph;
+
+/// A trait representing a generic graph.
+///
+/// This trait provides methods to manipulate vertices and edges in the graph, as well as to query
+/// information about them.
+///
+/// # Type Parameters
+///
+/// - `T`: The type of data stored in the vertices of the graph.
+///
 pub trait Graph<T: Clone>: Clone {
-	fn add_vertex(&mut self, data: T) -> usize;
-	fn remove_vertex(&mut self, vertex_id: usize);
+    /// Adds a new vertex to the graph with the specified data.
+    /// Returns the ID of the newly added vertex.
+    ///
+    /// # Arguments
+    ///
+    /// - `data`: The data to store in the vertex.
+    fn add_vertex(&mut self, data: T) -> usize;
 
-	fn add_edge(&mut self, vertex_id_1: usize, vertex_id_2: usize);
-	fn remove_edge(&mut self, vertex_id_1: usize, vertex_id_2: usize);
+    /// Removes the vertex with the specified ID from the graph.
+    /// Also removes all edges connected to the vertex.
+    ///
+    /// # Arguments
+    ///
+    /// - `vertex_id`: The ID of the vertex to remove.
+    fn remove_vertex(&mut self, vertex_id: usize);
 
-	fn get_vertex_data(&self, vertex_id: usize) -> Option<T>;
-	fn set_vertex_data(&mut self, vertex_id: usize, data: T);
+    /// Adds an edge between two vertices in the graph.
+    ///
+    /// # Arguments
+    ///
+    /// - `vertex_id_1`: The ID of the first vertex to connect.
+    /// - `vertex_id_2`: The ID of the second vertex to connect.
+    fn add_edge(&mut self, vertex_id_1: usize, vertex_id_2: usize);
 
-	fn is_adjacent(&self, vertex_id_1: usize, vertex_id_2: usize) -> bool;
-	fn get_neighbors(&self, vertex_id: usize) -> Vec<usize>;
+    /// Removes an edge between two vertices in the graph.
+    ///
+    /// Does nothing if the edge does not exist.
+
+    /// # Arguments
+    ///
+    /// - `vertex_id_1`: The ID of the first vertex to disconnect.
+    /// - `vertex_id_2`: The ID of the second vertex to disconnect.
+    fn remove_edge(&mut self, vertex_id_1: usize, vertex_id_2: usize);
+
+    /// Gets the data stored in a vertex with the specified ID.
+    /// Returns `None` if the vertex does not exist in the graph.
+    ///
+    /// # Arguments
+    ///
+    /// - `vertex_id`: The ID of the vertex to get the data for.
+    fn get_vertex_data(&self, vertex_id: usize) -> Option<T>;
+
+    /// Sets the data stored in a vertex with the specified ID.
+    ///
+    /// Does nothing if the vertex does not exist.
+    ///
+    /// # Arguments
+    ///
+    /// - `vertex_id`: The ID of the vertex to set the data for.
+    /// - `data`: The new data to store in the vertex.
+    fn set_vertex_data(&mut self, vertex_id: usize, data: T);
+
+    /// Determines whether two vertices in the graph are adjacent.
+    ///
+    /// Returns `true` if there is an edge between the two vertices, `false` if there is no edge or if the vertices don't exist.
+    ///
+    /// # Arguments
+    ///
+    /// - `vertex_id_1`: The ID of the first vertex to test.
+    /// - `vertex_id_2`: The ID of the second vertex to test.
+    fn is_adjacent(&self, vertex_id_1: usize, vertex_id_2: usize) -> bool;
+
+    /// Gets the IDs of all vertices connected to a given vertex.
+    ///
+    /// Returns a vector containing the IDs of all vertices that share an edge with the given vertex.
+    ///
+    /// # Arguments
+    ///
+    /// - `vertex_id`: The ID of the vertex to get the neighbors of.
+    fn get_neighbors(&self, vertex_id: usize) -> Vec<usize>;
 }
 
-pub trait WeightedGraph<T: Clone, W: Clone>: Graph<T> + Clone {
-	fn get_edge_weight(&self, vertex_id_1: usize, vertex_id_2: usize) -> W;
-	fn set_edge_weight(&mut self, vertex_id_1: usize, vertex_id_2: usize, weight: W);
+/// A trait representing a weighted graph.
+pub trait WeightedGraph<T: Clone, W: Clone>: Graph<T> {
+    /// Get the weight of the edge between two vertices.
+	/// Returns the weight of the edge between the two vertices, if the edge exists. Otherwise, returns `None`.
+    ///
+    /// # Arguments
+    ///
+    /// * `vertex_id_1` - The ID of the first vertex.
+    /// * `vertex_id_2` - The ID of the second vertex.
+    fn get_edge_weight(&self, vertex_id_1: usize, vertex_id_2: usize) -> Option<W>;
+
+    /// Set the weight of the edge between two vertices.
+    ///
+    /// # Arguments
+    ///
+    /// * `vertex_id_1` - The ID of the first vertex.
+    /// * `vertex_id_2` - The ID of the second vertex.
+    /// * `weight` - The weight to set.
+    fn set_edge_weight(&mut self, vertex_id_1: usize, vertex_id_2: usize, weight: W);
 }
--- src/undirected_sparse_graph.rs
+++ src/undirected_sparse_graph.rs
@@ -1,18 +1,16 @@
 use super::Graph;
 
-/// A graph data structure with undirected edges, implemented using an adjacency matrix.
-///
-/// A `UndirectedGraph` consists of a set of vertices and a set of edges connecting those vertices. Vertices are
-/// identified by a unique `usize` identifier, and have some associated data of type `T`. Edges are
-/// represented using an adjacency matrix, which stores a boolean value indicating whether there is an edge between
-/// each pair of vertices.
+/// The UndirectedSparseGraph struct represents an undirected sparse graph implemented
+/// using a variant of an adjacency list. The graph consists of a set of vertices, each of which
+/// has a unique usize identifier and some associated data of type T. The edges of the
+/// graph are represented using a vector of pairs of vertex identifiers.
 ///
 /// # Example
 ///
 /// ```
-/// use istos::{Graph, UndirectedGraph};
+/// use istos::{Graph, UndirectedSparseGraph};
 ///
-/// let mut graph: UndirectedGraph<()> = UndirectedGraph::new();
+/// let mut graph: UndirectedSparseGraph<()> = UndirectedSparseGraph::new();
 ///
 /// // Add some vertices and edges
 /// let v1 = graph.add_vertex(());
@@ -30,14 +28,14 @@ use super::Graph;
 /// println!("{:?}", graph);
 /// ```
 #[derive(Clone, Debug)]
-pub struct UndirectedGraph<T: Clone> {
+pub struct UndirectedSparseGraph<T: Clone> {
 	vertices: Vec<(usize, T)>, // A vector of vertex IDs and associated data
-	edges: Vec<bool>, // An adjacency matrix representing the edges between vertices
+	edges: Vec<(usize, usize)>, // An list of the edges between vertices
 	next_id: usize, // The ID to assign to the next added vertex
 }
 
-impl<T: Clone> UndirectedGraph<T> {
-	/// Create a blank UndirectedGraph.
+impl<T: Clone> UndirectedSparseGraph<T> {
+	/// Create a blank UndirectedSparseGraph.
 	pub fn new() -> Self {
 		Self {
 			vertices: vec![],
@@ -45,83 +43,34 @@ impl<T: Clone> UndirectedGraph<T> {
 			next_id: 0,
 		}
 	}
-
-	/// This function maps the (x, y) coordinates of a 2D matrix onto a 1D vector.
-	/// The mapping is done in a way that accounts for the fact that the adjacency matrix
-	/// of an undirected graph is symmetrical.
-	#[inline]
-	fn index_vector_with_coords(&self, x: usize, y: usize) -> usize {
-		if x <= y {
-			(self.vertices.len() * 2 - x - 1) * x / 2 + y
-		} else {
-			(self.vertices.len() * 2 - y - 1) * y / 2 + x
-		}
-	}
-
-	/// Utility function to find the index of a vertex from an id.
-	#[inline]
-	fn get_index_from_id(&self, id: usize) -> Option<usize> {
-		self.vertices.iter().position(|x| x.0 == id)
-	}
 }
 
-impl<T: Clone> Graph<T> for UndirectedGraph<T> {
+impl<T: Clone> Graph<T> for UndirectedSparseGraph<T> {
 	fn add_vertex(&mut self, data: T) -> usize {
 		// Get the next available vertex ID
 		let id: usize = self.next_id;
-		
-		// Insert a new row and column in the adjacency matrix for the new vertex
-		let size: usize = self.vertices.len();
-		for i in 0..=size {
-			self.edges.insert(size * i, false);
-		}
-		
+
 		// Add the new vertex to the vertices vector with its associated data
 		self.vertices.push((id, data));
-		
+
 		// Increment the next available vertex ID
 		self.next_id += 1;
-		
+
 		// Return the ID of the new vertex
 		id
 	}
 
 	fn remove_vertex(&mut self, vertex_id: usize) {
-		// Find the index of the vertex to be removed.
-		let Some(pos) = self.get_index_from_id(vertex_id) else { return; };
-
-		// Remove all edges connected to the vertex.
-		let size: usize = self.vertices.len();
-		for i in (0..size).rev() {
-			self.edges.remove(self.index_vector_with_coords(pos, i));
-		}
-
-		// Remove the vertex.
-		self.vertices.remove(pos);
+		self.vertices.retain(|x| x.0 != vertex_id);
+		self.edges.retain(|&x| x.0 != vertex_id && x.1 != vertex_id);
 	}
 
 	fn add_edge(&mut self, vertex_id_1: usize, vertex_id_2: usize) {
-		// Get the positions of the vertices in the vertex list
-		let Some(pos_1) = self.get_index_from_id(vertex_id_1) else { return; };
-		let Some(pos_2) = self.get_index_from_id(vertex_id_2) else { return; };
-
-		// Calculate the index of the corresponding edge in the `edges` vector
-		let index: usize = self.index_vector_with_coords(pos_1, pos_2);
-
-		// Set the value to true to indicate the presence of an edge
-		self.edges[index] = true;
+		self.edges.push((vertex_id_1, vertex_id_2));
 	}
 
 	fn remove_edge(&mut self, vertex_id_1: usize, vertex_id_2: usize) {
-		// Get the positions of the vertices in the vertex list
-		let Some(pos_1) = self.get_index_from_id(vertex_id_1) else { return; };
-		let Some(pos_2) = self.get_index_from_id(vertex_id_2) else { return; };
-
-		// Calculate the index of the corresponding edge in the `edges` vector
-		let index: usize = self.index_vector_with_coords(pos_1, pos_2);
-
-		// Set the value to false to indicate the absence of an edge
-		self.edges[index] = false;
+		self.edges.retain(|&x| x != (vertex_id_1, vertex_id_2) && x != (vertex_id_2, vertex_id_1));
 	}
 
 	fn get_vertex_data(&self, vertex_id: usize) -> Option<T> {
@@ -129,25 +78,16 @@ impl<T: Clone> Graph<T> for UndirectedGraph<T> {
 	}
 
 	fn set_vertex_data(&mut self, vertex_id: usize, data: T) {
-		let Some(pos) = self.get_index_from_id(vertex_id) else { return; };
-		self.vertices[pos].1 = data;
+		self.vertices.iter_mut().find(|x| x.0 == vertex_id).unwrap().1 = data;
 	}
 
 	fn is_adjacent(&self, vertex_id_1: usize, vertex_id_2: usize) -> bool {
-		// Get the positions of the vertices in the vertex list
-		let Some(pos_1) = self.get_index_from_id(vertex_id_1) else { return false; };
-		let Some(pos_2) = self.get_index_from_id(vertex_id_2) else { return false; };
-
-		// Calculate the index of the corresponding edge in the `edges` vector
-		let index: usize = self.index_vector_with_coords(pos_1, pos_2);
-
-		// Find the value of the edge at the index
-		self.edges[index]
+		self.edges.contains(&(vertex_id_1, vertex_id_2)) || self.edges.contains(&(vertex_id_2, vertex_id_1))
 	}
 
 	fn get_neighbors(&self, vertex_id: usize) -> Vec<usize> {
 		let mut res = Vec::new();
-
+		
 		// Iterate through all vertices to find neighbors of the given vertex
 		for i in 0..self.vertices.len() {
 			let other_id = self.vertices[i].0;
@@ -166,7 +106,7 @@ mod tests {
 
 	#[test]
 	fn test_add_vertex() {
-		let mut graph: UndirectedGraph<usize> = UndirectedGraph::new();
+		let mut graph: UndirectedSparseGraph<usize> = UndirectedSparseGraph::new();
 		let v1 = graph.add_vertex(1);
 		let v2 = graph.add_vertex(2);
 
@@ -177,7 +117,7 @@ mod tests {
 
 	#[test]
 	fn test_remove_vertex() {
-		let mut graph: UndirectedGraph<usize> = UndirectedGraph::new();
+		let mut graph: UndirectedSparseGraph<usize> = UndirectedSparseGraph::new();
 		let v1 = graph.add_vertex(1);
 		let v2 = graph.add_vertex(2);
 		let v3 = graph.add_vertex(3);
@@ -197,19 +137,20 @@ mod tests {
 
 	#[test]
 	fn test_add_edge() {
-		let mut graph: UndirectedGraph<usize> = UndirectedGraph::new();
+		let mut graph: UndirectedSparseGraph<usize> = UndirectedSparseGraph::new();
 		let v1 = graph.add_vertex(1);
 		let v2 = graph.add_vertex(2);
 
 		graph.add_edge(v1, v2);
 
+		assert_eq!(graph.edges.len(), 1);
 		assert_eq!(graph.is_adjacent(v1, v2), true);
 		assert_eq!(graph.is_adjacent(v2, v1), true);
 	}
 
 	#[test]
 	fn test_remove_edge() {
-		let mut graph: UndirectedGraph<usize> = UndirectedGraph::new();
+		let mut graph: UndirectedSparseGraph<usize> = UndirectedSparseGraph::new();
 		let v1 = graph.add_vertex(1);
 		let v2 = graph.add_vertex(2);
 		let v3 = graph.add_vertex(3);
@@ -219,6 +160,7 @@ mod tests {
 
 		graph.remove_edge(v1, v2);
 
+		assert_eq!(graph.edges.len(), 1);
 		assert_eq!(graph.is_adjacent(v1, v2), false);
 		assert_eq!(graph.is_adjacent(v2, v1), false);
 		assert_eq!(graph.is_adjacent(v2, v3), true);
@@ -226,7 +168,7 @@ mod tests {
 
 	#[test]
 	fn test_get_vertex_data() {
-		let mut graph: UndirectedGraph<usize> = UndirectedGraph::new();
+		let mut graph: UndirectedSparseGraph<usize> = UndirectedSparseGraph::new();
 		let v1 = graph.add_vertex(1);
 		let v2 = graph.add_vertex(2);
 
@@ -237,7 +179,7 @@ mod tests {
 
 	#[test]
 	fn test_set_vertex_data() {
-		let mut graph: UndirectedGraph<usize> = UndirectedGraph::new();
+		let mut graph: UndirectedSparseGraph<usize> = UndirectedSparseGraph::new();
 		let v1 = graph.add_vertex(1);
 		let v2 = graph.add_vertex(2);
 
@@ -249,7 +191,7 @@ mod tests {
 
 	#[test]
 	fn test_is_adjacent() {
-		let mut graph: UndirectedGraph<()> = UndirectedGraph::new();
+		let mut graph: UndirectedSparseGraph<()> = UndirectedSparseGraph::new();
 		let v1 = graph.add_vertex(());
 		let v2 = graph.add_vertex(());
 		let v3 = graph.add_vertex(());
@@ -282,7 +224,7 @@ mod tests {
 
 	#[test]
 	fn test_get_neighbors() {
-		let mut graph: UndirectedGraph<()> = UndirectedGraph::new();
+		let mut graph: UndirectedSparseGraph<()> = UndirectedSparseGraph::new();
 		let v1 = graph.add_vertex(());
 		let v2 = graph.add_vertex(());
 		let v3 = graph.add_vertex(());
